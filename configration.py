58852fd36dead718f831b6b16f2507b7ca9682ad
SHUBHAM THAKUR <shubhamwert@gmail.com>
2019-10-07 13:47:03 +0530

    databases

--- configration.py
+++ configration.py
@@ -1,4 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(".."))
+
+
 class Config(object):
         SECRET_KEY="#11WASD$$#BGD" or os.environ.get('SECRET_KEY')
+        SQLALCHEMYURL_DATABASE_URI=os.environ.get("DATABASE_URL") or \
+                'sqlite:///'+os.path.join(basedir,'app.db')
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
