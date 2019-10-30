075fa60a7879550fe1bf266fe4a5148dc178a318
Shivam Kapila <shivamkapila4@gmail.com>
2019-10-30 23:20:22 +0530

    Secret key removed
    Don't upload the secret key to Github. Remove the env configuration files too. The database can otherwise be exploited when you upload the file

--- configration.py
+++ configration.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(".."))
 
 
 class Config(object):
-        SECRET_KEY="#11WASD$$#BGD" or os.environ.get('SECRET_KEY')
+        SECRET_KEY="" or os.environ.get('SECRET_KEY') # Don't share secret key here. The Database can be exploited.
         SQLALCHEMYURL_DATABASE_URI=os.environ.get("DATABASE_URL") or \
                 'sqlite:///'+os.path.join(basedir,'app.db')
         SQLALCHEMY_TRACK_MODIFICATIONS = False
